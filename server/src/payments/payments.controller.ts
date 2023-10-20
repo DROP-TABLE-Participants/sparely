e5339fd7b351625593b03a538f166e400629ab61
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2023-10-20 21:56:04 +0300

    Add payments controller

---
+++ server/src/payments/payments.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { CreatePaymentDto } from './dto/create-payment.dto';
+import { UpdatePaymentDto } from './dto/update-payment.dto';
+
+@Controller('payments')
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post("donate")
+  donate(@Body() paymentData: CreatePaymentDto): void {
+    console.log(paymentData);
+  }
+}

1c3c81e46048592cb72013ee7320c3e57f9684c9
Stoyan Ivanov <ssivanov19@codingburgas.bg>
2023-10-22 02:01:00 +0300

    Update DB connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,21 +4,23 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { PaymentsModule } from './payments/payments.module';
 import { CampaignsModule } from './campaigns/campaigns.module';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "db",
-      port: 5432,
-      username: "sparely",
-      password: "sparely",
-      database: "sparely",
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
-      PaymentsModule, CampaignsModule],
+    PaymentsModule,
+    CampaignsModule,
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })

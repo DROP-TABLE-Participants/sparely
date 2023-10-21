a0876cebf18a60b6ac8aff201f7fb7dd664304cc
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2023-10-21 11:37:18 +0300

    Finish component design

--- client/src/components/CharityCard.tsx
+++ client/src/components/CharityCard.tsx
@@ -1,28 +1,12 @@
 import React from "react";
-import '../styles/components/CharityCard.scss'
-import { useNavigate } from "react-router-dom";
 
 
-
-interface Charity {
-    id: number;
-    imageUrl: string;
-    heading: string;
-    donatedAmount: number;
-}
-
-export const CharityCard = ({ charity }: { charity: Charity }) => {
-    const navigate = useNavigate();
-
+export const CharityCard = ({charity}:any) => {
     return (
-        <div className="charity-card" onClick={()=>{navigate(`/charity/${charity.id}`)}}>
-            <div className="image-container">
-                <img src={charity.imageUrl} alt="" />
-            </div>
-            <div className="info-container">
-                <h1 className="charity-heading">{charity.heading}</h1>
-                <p className="donated-amount">{charity.donatedAmount}€</p>
-            </div>
+        <div className="charity-card">
+           <h1>{charity.heading}</h1>
+            <p>{charity.donatedAmount}</p>
+            <img src={charity.image} alt="" />
         </div>
     )
 }
